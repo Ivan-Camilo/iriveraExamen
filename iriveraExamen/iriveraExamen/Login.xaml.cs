d4f14d8eccab71184c325e7dba9319792e8cfe05
Ivan-Camilo <ivan.rivera.s@gmail.com>
2023-04-25 18:48:48 -0500

    Segundo Commit. Estructura de Login casi terminada

---
+++ iriveraExamen/iriveraExamen/Login.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace iriveraExamen
+{	
+	public partial class Login : ContentPage
+	{	
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ iriveraExamen/iriveraExamen/PlainNumericEntryBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace iriveraExamen
+{
+	public class PlainNumericEntryBehavior : Behavior<Entry>
+	{
+        protected Action<Entry, string> AdditionalCheck;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += TextChanged_Handler;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+
+        protected virtual void TextChanged_Handler(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                return;
+            }
+            double value;
+            if (!double.TryParse(e.NewTextValue, out value))
+            {
+
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+
+            else
+                AdditionalCheck?.Invoke(((Entry)sender), e.OldTextValue);
+        }
+
+    }
+}
+
